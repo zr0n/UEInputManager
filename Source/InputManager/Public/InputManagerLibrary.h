0ade6eb9df5f21715bb1d0315a9baab1f49bed6b
Luiz Fernando <devluiz.fernando@gmail.com>
2018-03-28 12:14:05 -0300

    Fix Compiling error with UAT and License changes

--- Source/InputManager/Public/InputManagerLibrary.h
+++ Source/InputManager/Public/InputManagerLibrary.h
@@ -1,21 +1,10 @@
 /*
 * This file is part of the UEInputManager distribution (https://github.com/zr0n/UEInputManager or https://zr0n.github.io/blog/inputmanager/).
 * Copyright (c) 2018 Luiz Fernando.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, version 3.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
-
+#include "InputManagerModule.h"
+#include "InputManagerPrivatePCH.h"
 #include "CoreMinimal.h"
 #include "UObject/ObjectMacros.h"
 #include "Runtime/Engine/Classes/GameFramework/PlayerInput.h"
