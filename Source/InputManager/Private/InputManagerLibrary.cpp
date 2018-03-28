e690547c350a402e57a0d1befafb52a8d669fa6a
Luiz Fernando <devluiz.fernando@gmail.com>
2018-03-28 13:20:25 -0300

    Organizing stuff

--- Source/InputManager/Private/InputManagerLibrary.cpp
+++ Source/InputManager/Private/InputManagerLibrary.cpp
@@ -2,8 +2,8 @@
 * This file is part of the UEInputManager distribution (https://github.com/zr0n/UEInputManager or https://zr0n.github.io/blog/inputmanager/).
 * Copyright (c) 2018 Luiz Fernando.
 */
-#include "InputManagerModule.h"
 #include "InputManagerLibrary.h"
+#include "InputManagerModule.h"
 #include "InputManagerModule.h"
 
 
